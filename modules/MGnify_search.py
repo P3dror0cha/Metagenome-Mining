db83b7b638b7e0e27eac0d466edd1bfd7f0c1cb9
P3dror0cha <pedro.rodrigues.rocha@usp.br>
2025-10-19 13:10:45 -0400

    Refactor MGnify search function and output filenames

--- modules/MGnify_search.py
+++ modules/MGnify_search.py
@@ -14,10 +14,6 @@ def pesquisa_MGnify(max_pages):
         url = f"https://www.ebi.ac.uk/metagenomics/api/v1/biomes/root:Environmental:Aquatic:Marine/genomes?page={page}"
         resposta = requests.get(url)
 
-        if resposta.status_code != 200:
-            print(f"Erro {resposta.status_code} na página {page}")
-            break
-
         dados_json = resposta.json()
         ids_atuais = [item["id"] for item in dados_json.get("data", [])]
         lista_ids.extend(ids_atuais)
@@ -25,13 +21,14 @@ def pesquisa_MGnify(max_pages):
         print(f"Página {page}: {len(ids_atuais)} IDs encontrados")
         time.sleep(1)  
 
-    with open("aquatic_download.json", "w") as i:
+    with open("ids_metadata.json", "w") as i:
         json.dump(dados_json, i, indent=4)
 
-    with open("aquatic_ids.txt", "w") as f:
+    with open("list_of_ids.txt", "w") as f:
         for id_study in lista_ids:
             f.write(id_study + "\n")
 
     print(f"\nTotal de {len(lista_ids)} IDs coletados em {page} páginas.")
-    return "aquatic_download.json", lista_ids
+    return "ids_metadata.json", lista_ids
+
 
