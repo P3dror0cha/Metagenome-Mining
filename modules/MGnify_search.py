8f9eff176c49d39472d4762e75d223294efcfe7b
P3dror0cha <pedro.rodrigues.rocha@usp.br>
2025-10-19 13:39:29 -0400

    Refactor MGnify search function with error handling
    Updated the MGnify search function to handle errors and return JSON data.

--- modules/MGnify_search.py
+++ modules/MGnify_search.py
@@ -1,34 +1,40 @@
 import requests
 import json 
 
-def pesquisa_MGnify(max_pages):
+def MGnify_search(max_pages):
     """
-    Faz a pesquisa de vários IDs de amostras do MGnify.
-
+    Faz a pesquisa de vários IDs de amostras do MGnify. A pesquisa pode ser feita pelo catálogo de genomas do MGnify ou por bioma.
+    
     Parâmetros:
     max_pages: número máximo de páginas a buscar.
     """
     lista_ids = []
+    lista_json = []
 
     for page in range(1, max_pages + 1):
         url = f"https://www.ebi.ac.uk/metagenomics/api/v1/biomes/root:Environmental:Aquatic:Marine/genomes?page={page}"
         resposta = requests.get(url)
 
+        if resposta.status_code != 200:
+            print(f"Erro {resposta.status_code} na página {page}")
+            break
+
         dados_json = resposta.json()
         ids_atuais = [item["id"] for item in dados_json.get("data", [])]
+        lista_json.extend(dados_json.get("data", []))
         lista_ids.extend(ids_atuais)
 
         print(f"Página {page}: {len(ids_atuais)} IDs encontrados")
         time.sleep(1)  
 
-    with open("ids_metadata.json", "w") as i:
-        json.dump(dados_json, i, indent=4)
+    resultado_final_json = {"data": lista_json}
+    
+    with open("aquatic_download.json", "w") as i:
+        json.dump(resultado_final_json, i, indent=4)
 
-    with open("list_of_ids.txt", "w") as f:
+    with open("aquatic_ids.txt", "w") as f:
         for id_study in lista_ids:
             f.write(id_study + "\n")
 
     print(f"\nTotal de {len(lista_ids)} IDs coletados em {page} páginas.")
-    return "ids_metadata.json", lista_ids
-
-
+    return dados_json, lista_ids
